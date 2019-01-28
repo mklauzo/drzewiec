a3d5a939e886501ff017ed29a091f069b75198f7
Boba Fett <bargest@poczta.onet.pl>
2019-01-28 23:05:54 +0100

    w4

--- docker_cleaner.sh
+++ docker_cleaner.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 docker image rm -f registry.gitlab.com/maciej.klauzo/www_drzewiec
 docker container stop $(docker container ls -q)
-
 docker run -d -p 8085:80 registry.gitlab.com/maciej.klauzo/www_drzewiec:latest
