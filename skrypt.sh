71c53703682222090e34feef39d5aff16fb0a81c
Maciej Klauzo <3429529-maciej.klauzo@users.noreply.gitlab.com>
2019-01-31 14:40:18 +0000

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,3 +1,3 @@
 echo test
-docker container stop $(docker container ls -a -q)
-docker container rm $(docker container ls -a -q)
+#docker container stop $(docker container ls -a -q)
+#docker container rm $(docker container ls -a -q)
